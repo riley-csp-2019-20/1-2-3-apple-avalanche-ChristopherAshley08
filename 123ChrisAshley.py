b69f401fee41b9f4d3cdd5cc86bbdb5b23421d8d
Chris Ashley <55500786+ChristopherAshley08@users.noreply.github.com>
2019-11-21 10:47:47 -0500

    Add files via upload

--- 123ChrisAshley.py
+++ 123ChrisAshley.py
@@ -12,6 +12,8 @@ screen_width = 400
 screen_height = 400
 letter_list = ["A","B","C","D","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
+current_letter= "A"
+
 #Apple
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
@@ -26,11 +28,13 @@ wn.tracer(False)
 # given a turtle, set that turtle to be shaped by the image file
 
 def reset_apple(active_apple):
+  global current_letter
   length_of_list = len(letter_list)
   if (length_of_list != 0):
     index = rand.randint(0,length_of_list)
+    current_letter =  letter_list.pop(index)
     active_apple.goto(rand.randint(-(screen_width)/2, (screen_width)/2), rand.randint(-(screen_height)/2, (screen_height)/2))
-    draw_apple(active_apple, letter_list.pop(index))
+    draw_apple(active_apple, current_letter)
 
 def draw_apple(active_apple, letter):
   active_apple.shape(apple_image)
@@ -38,7 +42,7 @@ def draw_apple(active_apple, letter):
   draw_letter(letter,active_apple)
   wn.update()
 
-def applefall():
+def drop_apple():
   wn.tracer(True)
   apple.goto(apple.xcor(), ground_height)
   apple.clear()
@@ -53,33 +57,112 @@ def draw_letter (letter, active_apple):
   active_apple.write(letter, font=("arial", 74, "bold"))
   active_apple.setpos(remember_position)
 
+def check_letter_A():
+  if (current_letter == "A"):
+    drop_apple()
+def check_letter_B():
+  if (current_letter == "B"):
+    drop_apple()
+def check_letter_C():
+  if (current_letter == "C"):
+    drop_apple()
+def check_letter_D():
+  if (current_letter == "D"):
+    drop_apple()
+def check_letter_E():
+  if (current_letter == "E"):
+    drop_apple()
+def check_letter_F():
+  if (current_letter == "F"):
+    drop_apple()
+def check_letter_G():
+  if (current_letter == "G"):
+    drop_apple()
+def check_letter_H():
+  if (current_letter == "H"):
+    drop_apple()
+def check_letter_I():
+  if (current_letter == "I"):
+    drop_apple()
+def check_letter_J():
+  if (current_letter == "J"):
+    drop_apple()
+def check_letter_K():
+  if (current_letter == "K"):
+    drop_apple()
+def check_letter_L():
+  if (current_letter == "L"):
+    drop_apple()
+def check_letter_M():
+  if (current_letter == "M"):
+    drop_apple()
+def check_letter_N():
+  if (current_letter == "N"):
+    drop_apple()
+def check_letter_O():
+  if (current_letter == "O"):
+    drop_apple()
+def check_letter_P():
+  if (current_letter == "P"):
+    drop_apple()
+def check_letter_Q():
+  if (current_letter == "Q"):
+    drop_apple()
+def check_letter_R():
+  if (current_letter == "R"):
+    drop_apple()
+def check_letter_S():
+  if (current_letter == "S"):
+    drop_apple()
+def check_letter_T():
+  if (current_letter == "T"):
+    drop_apple()
+def check_letter_U():
+  if (current_letter == "U"):
+    drop_apple()
+def check_letter_V():
+  if (current_letter == "V"):
+    drop_apple()
+def check_letter_W():
+  if (current_letter == "W"):
+    drop_apple()
+def check_letter_X():
+  if (current_letter == "X"):
+    drop_apple()
+def check_letter_Y():
+  if (current_letter == "Y"):
+    drop_apple()
+def check_letter_Z():
+  if (current_letter == "Z" ):
+    drop_apple()
+
 draw_apple(apple,"A")
-wn.onkeypress(applefall,"a")
-wn.onkeypress(applefall,"b")
-wn.onkeypress(applefall,"c")
-wn.onkeypress(applefall,"d")
-wn.onkeypress(applefall,"e")
-wn.onkeypress(applefall,"f")
-wn.onkeypress(applefall,"g")
-wn.onkeypress(applefall,"h")
-wn.onkeypress(applefall,"i")
-wn.onkeypress(applefall,"j")
-wn.onkeypress(applefall,"k")
-wn.onkeypress(applefall,"l")
-wn.onkeypress(applefall,"m")
-wn.onkeypress(applefall,"n")
-wn.onkeypress(applefall,"o")
-wn.onkeypress(applefall,"p")
-wn.onkeypress(applefall,"q")
-wn.onkeypress(applefall,"r")
-wn.onkeypress(applefall,"s")
-wn.onkeypress(applefall,"t")
-wn.onkeypress(applefall,"u")
-wn.onkeypress(applefall,"v")
-wn.onkeypress(applefall,"w")
-wn.onkeypress(applefall,"x")
-wn.onkeypress(applefall,"y")
-wn.onkeypress(applefall,"z")
+wn.onkeypress(check_letter_A,"a")
+wn.onkeypress(check_letter_B,"b")
+wn.onkeypress(check_letter_C,"c")
+wn.onkeypress(check_letter_D,"d")
+wn.onkeypress(check_letter_E,"e")
+wn.onkeypress(check_letter_F,"f")
+wn.onkeypress(check_letter_G,"g")
+wn.onkeypress(check_letter_H,"h")
+wn.onkeypress(check_letter_I,"i")
+wn.onkeypress(check_letter_J,"j")
+wn.onkeypress(check_letter_K,"k")
+wn.onkeypress(check_letter_L,"l")
+wn.onkeypress(check_letter_M,"m")
+wn.onkeypress(check_letter_N,"n")
+wn.onkeypress(check_letter_O,"o")
+wn.onkeypress(check_letter_P,"p")
+wn.onkeypress(check_letter_Q,"q")
+wn.onkeypress(check_letter_R,"r")
+wn.onkeypress(check_letter_S,"s")
+wn.onkeypress(check_letter_T,"t")
+wn.onkeypress(check_letter_U,"u")
+wn.onkeypress(check_letter_V,"v")
+wn.onkeypress(check_letter_W,"w")
+wn.onkeypress(check_letter_X,"x")
+wn.onkeypress(check_letter_Y,"y")
+wn.onkeypress(check_letter_Z,"z")
 
 wn.listen()
 wn.mainloop()
